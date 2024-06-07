adbe7f9de65d46dde6c3b1d94c18868ec73ed8dd
Kaiffan <pavel.baev44@gmail.com>
2024-06-08 00:04:05 +0300

    Fast fix errors

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,11 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from authentication.views import RegistrationView, LoginView, LogoutView
+from authentication.views import registration_user, login_user, logout_user
 from django.urls import path
 
 urlpatterns = [
-    path('registrations/', RegistrationView.as_view(), name="registration_view"),
-    path('login/', LoginView.as_view(), name="login_view"),
-    path('logout/', LogoutView.as_view(), name="logout_view"),
+    path('registrations/', registration_user, name="registration_view"),
+    path('login/', login_user, name="login_view"),
+    path('logout/', logout_user, name="logout_view"),
     path('token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path('token/refresh/', TokenRefreshView.as_view(), name="token_refresh")
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -2,13 +2,12 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from authentication.serializers import RegistrationSerializer, LoginSerializer, LogoutSerializer
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.views import APIView
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
 
 
 @api_view(http_method_names=['POST'])
-@permission_classes(permission_classes=[IsAuthenticated, ])
+@permission_classes(permission_classes=[AllowAny, ])
 def registration_user(request: Request) -> Response:
     serializer = RegistrationSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
@@ -24,8 +23,8 @@ def registration_user(request: Request) -> Response:
 
 
 @api_view(http_method_names=['POST'])
-@permission_classes(permission_classes=[IsAuthenticated, ])
-def registration_user(request: Request) -> Response:
+@permission_classes(permission_classes=[AllowAny, ])
+def login_user(request: Request) -> Response:
     serializer = LoginSerializer(data=request.data)
     if not serializer.is_valid(raise_exception=True):
         print(serializer.errors)
@@ -40,7 +39,7 @@ def registration_user(request: Request) -> Response:
 
 @api_view(http_method_names=['POST'])
 @permission_classes(permission_classes=[IsAuthenticated, ])
-def registration_user(request: Request) -> Response:
+def logout_user(request: Request) -> Response:
     serializer = LogoutSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
